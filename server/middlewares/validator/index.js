da54ad49f9452d42c69ea5dddf51d76683a29aae
RobinSon991841 <robinson991841@outlook.com>
2025-02-03 01:43:52 +0900

    prettier

--- server/middlewares/validator/index.js
+++ server/middlewares/validator/index.js
@@ -2,8 +2,8 @@ const ProductBrand = require('../../models/ProductBrand');
 const ProductImages = require('../../models/ProductImages');
 const Category = require('../../models/Category');
 const _ = require('lodash');
-const path = require('path');
-const fs = require('fs');
+// const path = require('path');
+// const fs = require('fs');
 const { districts } = require('../common');
 const axios = require('axios');
 
